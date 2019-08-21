25e8cf9706c8794a7567ebc8aac10ac2cc5528a5
Farida Shebubakar <info@hellofarida.com>
2019-08-21 12:02:22 +1000

    MVC * Created DB, validation, foreign keys, controllers XO

---
+++ app/models/restaurant.rb
@@ -0,0 +1,9 @@
+class Restaurant < ApplicationRecord
+  has_many :reviews, dependent: :destroy
+
+  CATEGORY = %w[chinese italian japanese french belgian]
+
+  validates :name, uniqueness: true, presence: true
+  validates :address, uniqueness: true, presence: true
+  validates :category, inclusion: { in: CATEGORY }
+ end
